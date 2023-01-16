de867f165e24d0d6336ca90b069aebe8746ca395
Alex Oakley <alex@alexoakley.com>
2023-01-16 00:40:00 -0800

    Configured publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,18 @@ rootProject.name = "curve25519-kotlin"
 pluginManagement {
     val kotlinVersion: String by settings
     val dokkaVersion: String by settings
+    val nexusVersion: String by settings
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
-        `maven-publish`
         id("org.jetbrains.dokka") version dokkaVersion
+        id("io.github.gradle-nexus.publish-plugin") version nexusVersion
+        `maven-publish`
+        signing
     }
+
+    val ossrhUsername: String by settings
+    val ossrhPassword: String by settings
+    extra.set("ossrhUsername", ossrhUsername)
+    extra.set("ossrhPassword", ossrhPassword)
 }
